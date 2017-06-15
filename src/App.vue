1fa1f0b1c67db4bf2bf8060c410f71bf2bc1a4f5
Mixy <835410991@qq.com>
2017-06-15 15:21:14 +0800

    new file myvue
    new file myvue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" >
-    <h1 v-if="visi" v-bind:title="message">{{ message }}</h1>
-    <button v-on:click="reverseMessage">Reverse Message</button>
-      <ol>
-        <li v-for="todo in todos">
-          {{ todo.text }}
-        </li>
-      </ol>
- 
+    <img src="./assets/logo.png" > 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      message: 'hello vue' + new Date(),
-      visi: true,
-      todos: [
-        { text: 'Learn JavaScript' },
-        { text: 'Learn Vue' },
-        { text: 'Build something awesome' }
-      ],
-      methods: {
-        reverseMessage: function () {
-          this.message = this.message.split('').reverse().join('')
-        }
-      }
-    }
-  }
+  name: 'app'
 }
 
 </script>
