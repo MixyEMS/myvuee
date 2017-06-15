030d324f95d2f482a5b906d892ea85186332b61a
Mixy <835410991@qq.com>
2017-06-15 14:53:15 +0800

    v-bind
    v-bind demo in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" >
+    <h1 v-if="visi" v-bind:title="message">{{ message }}</h1>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+      <ol>
+        <li v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ol>
+ 
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      message: 'hello vue' + new Date(),
+      visi: true,
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ],
+      methods: {
+        reverseMessage: function () {
+          this.message = this.message.split('').reverse().join('')
+        }
+      }
+    }
+  }
 }
+
 </script>
 
 <style>
@@ -20,4 +45,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
